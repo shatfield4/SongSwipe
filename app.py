81db1c40324d7f40c6885dcd0b96fab70c2ec375
Justin Gonzales <jgonza1427@gmail.com>
2022-04-04 21:47:47 -0700

    Integrated spotipy

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
 from flask import Flask, render_template
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 app = Flask(__name__)
 
+scope = "user-library-read"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+
 @app.route("/")
-def hello_world():
+def hello_world():  
+    print("Hello")
+    # related = sp.artist_related_artists('spotify:artist:3jOstUTkEu2JkjvRdBA5Gu')
+    
+    results = sp.current_user_saved_tracks()
+    
+    for idx, item in enumerate(results['items']):
+        track = item['track']
+
+        print(idx, track['artists'][0]['name'], " – ", track['name'])
+        
     return render_template('index.html')
 
-@app.route("/reccomendations")
+
+
+
+@app.route("/recommendations")
 def reccomendations():
-    return render_template('reccomendations.html', )
+    return render_template('recommendations.html', )
