35d7ee582770236b49cb1ebe6134d4e6bd5e78bb
shatfield4 <seanhatfield5@gmail.com>
2022-04-11 21:33:49 -0700

    Added getReccomendationsFromArtist function

--- userfunctions.py
+++ userfunctions.py
@@ -35,10 +35,37 @@ def getArtistGenres(name):
     else:
         return None
 
+def getReccomendationFromArtist(artist):
+    artistGenreInfo = getArtistGenres(artist)
+    allReccomendationGenres = sp.recommendation_genre_seeds()
+
+    validGenres = []
+
+    if artistGenreInfo != None and len(artistGenreInfo) > 0:
+        # Loop through genres grabbed from calling getArtistGenres
+        for n in range(0, len(artistGenreInfo)):
+            currentGrabbedGenre = artistGenreInfo[n]
+            # Loop through all genres from spotify and add them to the validGenres dictionary if there is a match
+            for x in range(0, len(allReccomendationGenres['genres'])):
+                if currentGrabbedGenre == allReccomendationGenres['genres'][x]:
+                    validGenres.append(currentGrabbedGenre)
+    else:
+        print('No results found...')
+
+    print('Valid Genres: ' + str(validGenres))
+
+    if len(validGenres) > 0:
+        print('\n\n\nReccomendations:')
+        pp.pprint(sp.recommendations(seed_genres=validGenres))
+        print('\n\n\n')
+
+
+
 artist_name = input("Input artist name: ")
 
 pp.pprint(getArtist(artist_name))
-print("\n")
-pp.pprint(getArtistGenres(artist_name))
+print("\n\n\n\n")
+getReccomendationFromArtist(artist_name)
+# pp.pprint(getReccomendationFromArtist(artist_name))
 
-print(sp.recommendation_genre_seeds())
+# print(sp.recommendation_genre_seeds())
