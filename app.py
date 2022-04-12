a46fd21acbf069ca1e135eab77484d1f61d0a76d
Sean Del Castillo <seanbdcastillo@gmail.com>
2022-04-11 20:49:55 -0700

    Added ClientCredentials Auth to app.py

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv, find_dotenv
+import os
 
 app = Flask(__name__)
 
-scope = "user-library-read"
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+# Load env variables
+load_dotenv(find_dotenv())
 
+# Env Variables
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+REDIRECT_URI = os.environ.get("REDIRECT_URI")
+
+# Intialize spotipy instance
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 @app.route("/")
 def hello_world():  
