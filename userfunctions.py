0eb380de03533664312fc96a6d65fe77f2120a69
Justin Gonzales <jgonza1427@gmail.com>
2022-05-02 20:51:30 -0700

    Changed userfunctions.py authorization
    Now able to retrieve user information

--- userfunctions.py
+++ userfunctions.py
@@ -17,16 +17,26 @@ CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 REDIRECT_URI = os.environ.get("REDIRECT_URI")
 
 # scope = 'user-read-playback-state'
+scope = 'user-follow-read'
 # Intialize spotipy instance
 
-client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-# oAuth = auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, scope=scope, redirect_uri=REDIRECT_URI)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+# client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+oAuth = auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, scope=scope, redirect_uri=REDIRECT_URI)
+sp = spotipy.Spotify(auth_manager=oAuth)
 
 pp = pprint.PrettyPrinter(indent=2)
 
 sqlite = Sqlite_test(r'database.db')
 
+
+
+def getArtist(name):
+    results = sp.search(q='artist:' + name, type='artist')
+    items = results['artists']['items']
+    if len(items) > 0:
+        return items[0]
+    else:
+        return None
 
 def getArtistGenres(name):
     results = sp.search(q='artist:' + name, type='artist')
@@ -40,7 +50,6 @@ def getReccomendationFromArtist(artist):
     artistGenreInfo = getArtistGenres(artist)
     allReccomendationGenres = sp.recommendation_genre_seeds()
 
-    # Find all genres that match the artist to all genres
     validGenres = []
 
     if artistGenreInfo != None and len(artistGenreInfo) > 0:
@@ -54,32 +63,22 @@ def getReccomendationFromArtist(artist):
     else:
         print('No results found...')
 
-    # Get song reccomendations from the artist specified
+    print('Valid Genres: ' + str(validGenres))
+
     if len(validGenres) > 0:
         print('\n\n\nReccomendations:')
-
-        reccomendationsOutput = sp.recommendations(seed_genres=validGenres)
-
-        songReccomendationIds = []
-        songReccomendationNames = []
-        songPreviewUrls = []
-
-        # Get song names and IDs from spotipy
-        for x in range(0, len(reccomendationsOutput['tracks'])):
-            songReccomendationIds.append(reccomendationsOutput['tracks'][x]['id'])
-            songReccomendationNames.append(reccomendationsOutput['tracks'][x]['name'])
-            songPreviewUrls.append(reccomendationsOutput['tracks'][x]['preview_url'])
-
-
-        pp.pprint(songReccomendationIds)
-        pp.pprint(songReccomendationNames)
-        pp.pprint(songPreviewUrls)
-
-
-        # pp.pprint(sp.recommendations(seed_genres=validGenres)['tracks'][0]['name'])
+        for i in range(10):
+            pp.pprint(sp.recommendations(seed_genres=validGenres)['tracks'][i]['artists'][0]['name'])
+    
+        # pp.pprint(sp.recommendations(seed_genres=validGenres))
+        
         print('\n\n\n')
 
+def getUsername():
+    print(sp.current_user()['display_name'])
 
+def getFollowedArtists():
+    pp.pprint(sp.current_user_followed_artists())
 
 
 
@@ -91,9 +90,15 @@ if __name__ == '__main__':
     # sqlite.removeArtist("SZA")
     # sqlite.cursor.execute("DROP TABLE Song")
 
-
-    artist_name = input("Input artist name: ")
+    
+    getUsername()
+    
+    getFollowedArtists()
+    
+    
+    # pp.pprint(sp.current_user()['display_name'])
+    # artist_name = input("Input artist name: ")
     # pp.pprint(getArtist(artist_name))
-    print("\n\n\n\n")
-    getReccomendationFromArtist(artist_name)
+    # print("\n\n\n\n")
+    # getReccomendationFromArtist(artist_name)
 
