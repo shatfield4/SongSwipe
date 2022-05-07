385547898a7d29fe67fe5555a52c02c513a86fa4
Monte D <monte@csu.fullerton.edu>
2022-05-07 12:18:31 -0700

    Trying to merge

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, jsonify, request, render_template, url_for
-import spotipy
+from flask import Flask, jsonify, request, render_template, url_for, redirect
+import spotipy, requests, json
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv, find_dotenv
 import os
 import userfunctions
 from flask_sqlalchemy import SQLAlchemy
+from getToken import getUser
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
 # Honestly idk this starts sqlite or something
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -57,5 +60,52 @@ def api_postSwipeRight():
 
     return jsonify(dictToReturn)
 
-if __name__ == '__main__':
+@app.route("/")
+def index():  
+    return render_template("index.html")
+
+@app.route('/auth/')
+def auth():
+    response = getUser(CLIENT_ID, REDIRECT_URI, 5000)
+    return redirect(response)
+
+@cross_origin(supports_credentials=True)
+@app.route('/data/')
+def data():
+    liked_artist = request.args['artist_liked']
+    response = {"name": "Justin Bieber", "img_url": "https://i.scdn.co/image/ab676161000051748ae7f2aaa9817a704a87ea36", "song_url": "spotify:artist:1uNFoZAHBGtllmzznpCI3s", "genre": "Pop"}
+    
+    return jsonify(response)
+
+@app.route('/callback/')
+def callback():
+    # Auth Step 4: Requests refresh and access tokens
+    SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
+
+    REDIRECT_URI_ = REDIRECT_URI + ":5000/callback/"
+
+    auth_token = request.args['code']
+
+    code_payload = {
+        "grant_type": "authorization_code",
+        "code": auth_token,
+        "redirect_uri": REDIRECT_URI_,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+    }
+
+    post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload)
+
+    # Auth Step 5: Tokens are Returned to Application
+    response_data = json.loads(post_request.text)
+
+    access_token = response_data["access_token"]
+    refresh_token = response_data["refresh_token"]
+    token_type = response_data["token_type"]
+    expires_in = response_data["expires_in"]
+
+    #return {"access_token": access_token}
+    return redirect(f"http://localhost:3000?access_token={access_token}", code=307)
+
+if __name__ == "__main__":
     app.run(debug=True)
